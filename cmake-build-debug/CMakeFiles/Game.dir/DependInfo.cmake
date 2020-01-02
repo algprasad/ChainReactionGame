a87621657d1b9b52be1260f6691b5d14ef428e7d
algprasad <algprasad@outlook.com>
2020-01-03 02:50:48 +0530

    changed the exclude file

---
+++ cmake-build-debug/CMakeFiles/Game.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alg/CLionProjects/ChainReactionGame/Box.cpp" "/home/alg/CLionProjects/ChainReactionGame/cmake-build-debug/CMakeFiles/Game.dir/Box.cpp.o"
+  "/home/alg/CLionProjects/ChainReactionGame/main.cpp" "/home/alg/CLionProjects/ChainReactionGame/cmake-build-debug/CMakeFiles/Game.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/alg/Documents/SFML-2.5.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
